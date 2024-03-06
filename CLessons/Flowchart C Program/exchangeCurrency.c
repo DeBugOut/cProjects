04b98dabc7dc7b5d391ee7893ecc5cdb599c600d
S-L-9999 <hongsovanleonan@email.com>
2024-03-06 22:30:21 +0700

    Comment

--- CLessons/Flowchart C Program/exchangeCurrency.c
+++ CLessons/Flowchart C Program/exchangeCurrency.c
@@ -5,6 +5,7 @@
 
 //Function converts USD to EUR & JPY
 int main(){
+    //Using floats for decimals
     float amountUSD = 0;
     printf("What is the amount of cash in USD?\n");
     scanf("%f", &amountUSD);
@@ -15,6 +16,7 @@ int main(){
     const float rateJPY = 150.54;
     amountEUR = amountUSD * rateEUR;
     amountJPY = amountUSD * rateJPY;
+    // 2 digits
     printf("The amount of money in USD is: %.02f\n", amountUSD);
     printf("The amount of money in EUR is: %.02f\n", amountEUR);
     printf("The amount of money in JPY is: %.02f\n", amountJPY);
