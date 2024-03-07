068e5214ac3773656d2331c57a074007344a6909
S-L-9999 <hongsovanleonan@email.com>
2024-03-07 19:10:46 +0700

    Completed 2.19

--- CLessons/CLessons/nineteenProblem.c
+++ CLessons/CLessons/nineteenProblem.c
@@ -5,43 +5,68 @@
 
 int main(void){
     int number1, number2, number3;
-    printf("What are the three numbers?\n");
+    printf("Enter three different integers: ");
     scanf("%d %d %d", &number1, &number2, &number3);
 
     int sum = 0;
     sum = number1 + number2 + number3;
-    printf("The sum is %d\n", sum);
+    printf("Sum is %d\n", sum);
     
     int avg = 0;
     avg = sum / 3;
-    printf("The average is %d\n", avg);
+    printf("Average is %d\n", avg);
 
     int product = 0;
     product = number1 * number2 * number3;
-    printf("The product is %d\n", product);
+    printf("Product is %d\n", product);
 
     if (number1 < number2){
         if (number1 < number3){
-            printf("The smallest number is %d\n", number1);
+            printf("Smallest is %d\n", number1);
         }
         if (number1 > number3){
-            printf("The smallest number is %d\n", number3);
+            printf("Smallest is %d\n", number3);
         }
     }
     if (number2 < number1){
         if (number2 < number3){
-            printf("The smallest number is %d\n", number2);
+            printf("Smallest is %d\n", number2);
         }
         if (number2 > number3){
-            printf("The smallest number is %d\n", number3);
+            printf("Smallest is %d\n", number3);
         }
     }
     if (number3 < number1){
         if (number3 < number2){
-            printf("The smallest number is %d\n", number3);
+            printf("Smallest is %d\n", number3);
         }
         if (number3 > number2){
-            printf("The smallest number is %d\n", number2);
+            printf("Smallest is %d\n", number2);
+        }
+    }
+
+    if (number1 > number2){
+        if (number1 < number3){
+            printf("Largest is %d\n", number3);
+        }
+        if (number1 > number3){
+            printf("Largest is %d\n", number1);
+        }
+    }
+    if (number2 > number1){
+        if (number2 < number3){
+            printf("Largest is %d\n", number3);
+        }
+        if (number2 > number3){
+            printf("Largest is %d\n", number2);
+        }
+    }
+    if (number3 < number1){
+        if (number3 < number2){
+            printf("Largest is %d\n", number2);
+        }
+        if (number3 > number2){
+            printf("Largest is %d\n", number3);
         }
     }
     return 0;
