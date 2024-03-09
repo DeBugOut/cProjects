bfe3f62d7111d9d3eafaa0f0ed7934d946f18ba5
S-L-9999 <hongsovanleonan@email.com>
2024-03-09 16:39:26 +0700

    C Lessons for string lesson and functions.

--- Learning C/CLessons/CLesson.c
+++ Learning C/CLessons/CLesson.c
@@ -1,7 +1,16 @@
 // CLessons.c
 // Lessons on C
 
-#include <stdio.h>
+#include <stdio.h>      //gives access to printf and scanf
+#include <stdbool.h>    //gives access to booleans
+#include <math.h>       //gives access to math library
+#include <string.h>     //gives access to string editting. 
+
+//FUNCTION Prototype
+//used to declare a function before the body.
+void birthday(char[], int);
+
+int findMax(int,int);
 
 int main(){
     // This is a program that outputs "Hello World"
@@ -81,5 +90,84 @@ int main(){
 
     printf("%d", t);
 
+    //logical operators - && (means 'and' operator)
+    //logical operators - || (means 'or' operator)
+    //logical operators - ! (means 'not' operator)
+
+    int temp = 25;
+    bool sunny = true; 
+
+    //'and' logical operator (&&) checks if
+    //both conditions are true. 
+    if (temp <= 35 && temp > 0 && sunny){
+        printf("\nThe weather is good!");
+    }
+    else{
+        printf("\nThe weather is bad!");
+    } 
+
+    //Example of logical operator 'and' (&&)
+
+    //'or' logical operator (||) checks if
+    //one of the conditions are true. 
+    if (temp <= 0 || temp >= 35){
+        printf("\nThe weather is bad");
+    }
+    else{
+        printf("\nThe weather is good");
+    }
+
+    //Example of logical operator 'or' (||)
+
+    //'not' logical operator (!) checks if
+    //a condition is false. 
+    if (!sunny){
+        printf("\nIt's cloudy.");
+    }
+    else{
+        printf("\nIt's sunny.");
+    }
+
+    //Arguments and Parameters:
+    char chmous[] = "Leo";
+    int ayu = 18;
+
+    //Calling the FUNCTION Birthday:
+    birthday(chmous, ayu);
+
+    //ternary operator - short cut for an if/else statement
+    //(condition) ? value if true : value if false
+
+    int max = findMax(3, 5);
+
+    printf("The max value is: %d\n", max);
+
+    char string1[] = "Bro";
+    char string2[] = "Program";
+
+    //FUNCTIONS of string.h:
+    //strlwr(string1);              //converts to lower case
+    //strupr(string1);              //converts to upper case
+    //strcat(string1, string2);     //appends str2 to the end of str1
+    //strncat(string1, string2, 1); //same thing except append n characters
+    //strcpy(string1, string2);     //copy string2 to string1
+    //strncpy(string1, string2, 1); //same thing except copy n characters
+
+    printf("%s", string1);
+
     return 0;
+}
+
+//FUNCTIONS in C:
+void birthday(char x[], int y)
+{
+    printf("\nHappy Birthday to %s!\n", x);
+    printf("Happy Birthday to %s!\n", x);
+    printf("You are %d years old now!\n", y);
+    printf("You are %d years old now!\n", y);
+}
+
+int findMax(int x, int y)
+{
+    return (x>y) ? x : y;
 }
