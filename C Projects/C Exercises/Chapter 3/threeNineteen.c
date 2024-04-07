a56f6d854f2ca2667d6b33a6dfe0ca9e552bda90
S-L-9999 <hongsovanleonan@gmail.com>
2024-04-07 18:37:25 +0700

    3.19

---
+++ C Projects/C Exercises/Chapter 3/threeNineteen.c
@@ -0,0 +1,33 @@
+//3.19
+//Interest Calculator
+
+#include <stdio.h>
+
+float interest(float x, float y, int z){
+    float charge = x * y * z / 365;
+    return charge;
+}
+
+int main(){
+    float principal, rate;
+    int days;
+
+    printf("Enter loan principal (-1 to end): ");
+    scanf("%f", &principal);
+
+    while (principal != -1){
+        printf("Enter interest rate: ");
+        scanf("%f", &rate);
+
+        printf("Enter term of the loan in days: ");
+        scanf("%d", &days);
+
+        printf("The interest charge is $%.2f\n\n", interest(principal, rate, days));  
+
+        printf("Enter loan principal (-1 to end): ");
+        scanf("%f", &principal);
+      
+    }
+ 
+    return 0;
+}
