6f55d9fb9f7dee692028d395574b775c245aad69
S-L-9999 <hongsovanleonan@gmail.com>
2024-05-07 11:11:32 +0700

    5.15 - Completed.

---
+++ C Projects/C Exercises/Chapter 5/fiveFIfteen.c
@@ -0,0 +1,24 @@
+//fiveFifteen.c
+// Hypothenuse Calculation Porgram.
+
+#include <stdio.h>
+#include <math.h>
+
+double hypothenuse(double x, double y);
+
+int main(){
+    // section A:
+    double side1, side2;
+
+    printf("Input 2 sides of a triangle to find the hypothenuse: ");
+    scanf("%lf %lf", &side1, &side2);
+
+    printf("The hypothenuse is: %.02lf", hypothenuse(side1, side2));
+
+    return 0;
+}
+
+double hypothenuse(double x, double y){
+    double c = sqrt(x*x + y*y);
+    return c;
+}
